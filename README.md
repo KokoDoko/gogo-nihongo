b31f0f96fe36bfe67b451d29f18c53f2ab6386cd
Erik <erik@erik-hr.lan>
2018-11-30 18:36:40 +0100

    Works with CSV

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # Gogo Nihongo
 
 A chrome extension that replaces English words with the first basic kanji or kana characters from your study book. 
-The extension should search any html page for words mentioned in the file `words.js`. These words are then replaced with the accompanying kana/kanji.
-
-```
-let words = [["water", "みず"], ["tree", "き"], ["sun", "ひ"], ["forest", "もり"]]
-```
+The extension replaces words mentioned in the file `words.csv` with kanji.
 
 ## Testing
 
 - Download this repository. 
-- Edit the `words.js` file to add new words
+- Generate a new `words.csv` file to add new words
 - Open the Chrome extensions page, enable developer mode
 - Click "Load Unpacked" and choose the folder you just downloaded
 
 ## TODO
 
-- Styling
-- Create lists with the first 100 words you learn, in kana and in kanji
-- User can select lists
-- User can input own words
-- Put lists online instead of in the extension itself
-- Save as CRX file and put in the chrome store
+- Extension icon
+- Styling, toolip for the kanji
+- The CSV file has to be loaded remotely
+- Create CSV files for JLPT 5, 4
+- User can select JLPT version
+- User can add words locally
+- Save the extension as a CRX file, put in the chrome store
---
+++ main.js
@@ -0,0 +1,80 @@
+let words = []
+
+// ************************************************
+//
+// go through the DOM to find text nodes
+//
+// ************************************************
+function walk(node) 
+{
+	let child, next
+	
+	if (node.tagName && (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+		|| node.tagName.toLowerCase() == 'script' || node.tagName.toLowerCase() == 'style')) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild
+			
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child)
+				child = next
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node)
+			break
+	}
+}
+
+// ************************************************
+//
+// check if this word is in the CSV file and replace it with the kanji
+//
+// ************************************************
+function handleText(textNode) {
+	let v = textNode.nodeValue
+
+	for (let w of words) {
+		var regex = new RegExp("\\b(" + w[0] + ")\\b", "gi");
+
+		v = v.replace(regex, w[1]);
+	}
+
+	// TODO hover mouse to see original word / kana
+
+	textNode.nodeValue = v
+}
+
+// ************************************************
+//
+// start app
+//
+// ************************************************
+function startApp(){
+	// url inside chrome extension
+	const url = chrome.runtime.getURL("words.csv")
+	//console.log(url)
+
+	Papa.parse(url, {
+		download: true,
+		skipEmptyLines: true,
+		complete: showResults
+	})
+}
+
+function showResults(obj) {
+	words = obj.data
+	words.shift()
+	walk(document.body)
+}
+
+startApp()
